870b6b6a5ba169f67feeb51f7df8b3b50d28dc0b
maurits <info@mauritsbecks.com>
2023-04-15 11:03:12 +0200

    Remove master

--- invoicing/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE invoice (
-    id BIGINT PRIMARY KEY,
-    invoice_id VARCHAR(255),
-    customer_id VARCHAR(255),
-    booking_id VARCHAR(255),
-    status VARCHAR(255),
-    total_price DECIMAL(10,2)
-);
--- location/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Location (Name, Description, Rental_Duration, Rental_Rate, Rating, Special_Features, `Fulltext`, Last_Update)
-VALUES 
-('Resort Hotel Bad Boekelo', 'Luxury resort hotel located in the countryside near Enschede', 7, 150, 4, 'Spa, outdoor pool, tennis court', 'Resort Hotel Bad Boekelo Enschede', NOW()),
-('Kinepolis Enschede', 'Modern cinema complex located in the city center of Enschede', 3, 10, 4, 'IMAX, Dolby Atmos, 3D', 'Kinepolis Enschede cinema', NOW()),
-('Vliegveld Twenthe Evenementenlocatie', 'Event venue located at Twenthe airport', 1, 500, 5, 'Hangar space, outdoor area, catering', 'Vliegveld Twenthe event venue', NOW()),
-('City Hotel Hengelo', 'City hotel located in the center of Hengelo', 2, 75, 3, 'Breakfast included, free wifi', 'City Hotel Hengelo', NOW()),
-('Spinnerij Oosterveld', 'Industrial complex converted into modern office space', 12, 500, 4, 'High ceilings, natural light, parking', 'Spinnerij Oosterveld office space', NOW()),
-('Fletcher Hotel-Restaurant De Broeierd Enschede', 'Boutique hotel located in an 19th-century farmhouse', 3, 100, 4, 'Gourmet restaurant, free parking', 'De Broeierd hotel Enschede', NOW()),
-('De Oude Usselerschool', 'Historic school building converted into event space', 1, 250, 4, 'Original details, catering options', 'De Oude Usselerschool event space', NOW());
